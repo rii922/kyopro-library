c8b8a2d350a460314cd90d6cff36f6dd52c51dd9
rii922 <147631072+rii922@users.noreply.github.com>
2024-09-05 17:58:02 +0900

    Add files via upload

--- convolution.cpp
+++ convolution.cpp
@@ -2,94 +2,211 @@
 using namespace std;
 
 template<int mod>
-struct static_convolution {
+struct ntt_friendly_convolution {
 	using mint = static_mint<mod>;
-	static_convolution(int n) {
-		_n = n;
-		_n2 = 1;
-		while (_n2 < _n) _n2 *= 2;
-		assert((mod - 1) % _n2 == 0);
-		_init_primes();
-		_root.resize(_n2+1);
-		mint g = _primitive_root().pow((mod-1)/_n2);
-		_root[0] = 1;
-		for (int i = 0; i < _n2; i++) _root[i+1] = _root[i] * g;
-	}
-	vector<mint> operator()(vector<mint> &a, vector<mint> &b) {
+	ntt_friendly_convolution() {}
+	vector<mint> operator()(vector<mint> &a, vector<mint> &b) const {
 		int n = a.size() + b.size() - 1;
-		assert(n <= _n);
-		vector<mint> a2(_n2, 0), b2(_n2, 0);
+		int n2 = 1;
+		while (n2 < n) n2 *= 2;
+		assert((mod - 1) % n2 == 0);
+		vector<mint> root(n2+1), a2(n2, 0), b2(n2, 0);
+		root[0] = 1;
+		mint g = PR.pow((mod-1)/n2);
+		for (int i = 0; i < n2; i++) root[i+1] = root[i] * g;
 		for (int i = 0; i < a.size(); i++) a2[i] = a[i];
 		for (int i = 0; i < b.size(); i++) b2[i] = b[i];
-		_ntt(a2);
-		_ntt(b2);
-		for (int i = 0; i < _n2; i++) a2[i] *= b2[i];
-		_ntt_inv(a2);
+		_ntt(a2, root);
+		_ntt(b2, root);
+		for (int i = 0; i < n2; i++) a2[i] *= b2[i];
+		_intt(a2, root);
+		mint ni = mint(n2).inv();
 		vector<mint> c(n);
-		for (int i = 0; i < n; i++) c[i] = a2[i];
+		for (int i = 0; i < n; i++) c[i] = a2[i] * ni;
+		return c;
+	}
+	vector<int> operator()(vector<int> &a, vector<int> &b) const {
+		int n = a.size() + b.size() - 1;
+		int n2 = 1;
+		while (n2 < n) n2 *= 2;
+		assert((mod - 1) % n2 == 0);
+		vector<mint> root(n2+1), a2(n2, 0), b2(n2, 0);
+		root[0] = 1;
+		mint g = PR.pow((mod-1)/n2);
+		for (int i = 0; i < n2; i++) root[i+1] = root[i] * g;
+		for (int i = 0; i < a.size(); i++) a2[i] = a[i];
+		for (int i = 0; i < b.size(); i++) b2[i] = b[i];
+		_ntt(a2, root);
+		_ntt(b2, root);
+		for (int i = 0; i < n2; i++) a2[i] *= b2[i];
+		_intt(a2, root);
+		mint ni = mint(n2).inv();
+		vector<int> c(n);
+		for (int i = 0; i < n; i++) c[i] = (a2[i] * ni).val();
 		return c;
 	}
 private:
-	int _n, _n2;
-	const mint INV2 = mint(2).inv();
-	vector<int> _primes;
-	vector<mint> _root;
-	void _init_primes() {
-		int x = mod - 1;
-		for (int i = 2; (long long)i * i <= x; i++) {
-			if (x % i == 0) {
-				_primes.push_back(i);
-				while (x % i == 0) x /= i;
-			}
+	static const mint PR;
+	void _ntt(vector<mint> &v, vector<mint> &root) const {
+		if (v.size() <= 1) return;
+		if (v.size() == 2) {
+			mint v0 = v[0];
+			v[0] = v0 + v[1];
+			v[1] = v0 - v[1];
+			return;
 		}
-		if (x > 1) _primes.push_back(x);
-	}
-	mint _primitive_root() {
-		mt19937 mt(time(nullptr));
-		while (true) {
-			mint g = mt();
-			bool ok = true;
-			for (auto &x : _primes) {
-				if (g.pow((mod-1)/x) == 1) {
-					ok = false;
-					break;
+		int e = __builtin_ctz(v.size());
+		int d = e;
+		int n2 = 1 << e;
+		if (e & 1) {
+			int b = n2 >> 1;
+			for (int i = 0; i < b; i++) {
+				mint s = v[i];
+				mint t = v[i+b];
+				v[i] = s + t;
+				v[i+b] = (s - t) * root[n2-i];
+			}
+			d--;
+		}
+		int b = 1 << (d - 2);
+		while (d >= 2) {
+			for (int i = 0; i < n2; i += (b << 2)) {
+				for (int j = 0; j < b; j++) {
+					int p0 = i + j;
+					int p1 = p0 + b;
+					int p2 = p1 + b;
+					int p3 = p2 + b;
+					mint t0 = v[p0];
+					mint t1 = v[p1];
+					mint t2 = v[p2];
+					mint t3 = v[p3];
+					mint t0p2 = t0 + t2;
+					mint t1p3 = t1 + t3;
+					mint t0m2 = (t0 - t2) * root[n2-(j<<(e-d))];
+					mint t1m3 = (t1 - t3) * root[n2-((j+b)<<(e-d))];
+					v[p0] = t0p2 + t1p3;
+					v[p1] = (t0p2 - t1p3) * root[n2-(j<<(e-d+1))];
+					v[p2] = t0m2 + t1m3;
+					v[p3] = (t0m2 - t1m3) * root[n2-(j<<(e-d+1))];
 				}
 			}
-			if (ok) return g;
+			d -= 2;
+			b >>= 2;
 		}
 	}
-	void _ntt(vector<mint> &v) {
-		int n = v.size();
-		if (n == 1) return;
-		vector<mint> even(n/2), odd(n/2);
-		for (int i = 0; i < n/2; i++) {
-			even[i] = v[i*2];
-			odd[i] = v[i*2+1];
+	void _intt(vector<mint> &v, vector<mint> &root) const {
+		if (v.size() <= 1) return;
+		if (v.size() == 2) {
+			mint v0 = v[0];
+			v[0] = v0 + v[1];
+			v[1] = v0 - v[1];
+			return;
 		}
-		_ntt(even);
-		_ntt(odd);
-		for (int i = 0; i < n/2; i++) {
-			odd[i] *= _root[_n2/n*i];
-			v[i] = even[i] + odd[i];
-			v[n/2+i] = even[i] - odd[i];
+		int e = __builtin_ctz(v.size());
+		int d = 2;
+		int n2 = 1 << e;
+		int b = 1;
+		while (d <= e) {
+			for (int i = 0; i < n2; i += (b << 2)) {
+				for (int j = 0; j < b; j++) {
+					int p0 = i + j;
+					int p1 = p0 + b;
+					int p2 = p1 + b;
+					int p3 = p2 + b;
+					mint t0 = v[p0];
+					mint t1 = v[p1] * root[j<<(e-d+1)];
+					mint t2 = v[p2];
+					mint t3 = v[p3] * root[j<<(e-d+1)];
+					mint t0p1 = t0 + t1;
+					mint t2p3 = (t2 + t3) * root[j<<(e-d)];
+					mint t0m1 = t0 - t1;
+					mint t2m3 = (t2 - t3) * root[(j+b)<<(e-d)];
+					v[p0] = t0p1 + t2p3;
+					v[p1] = t0m1 + t2m3;
+					v[p2] = t0p1 - t2p3;
+					v[p3] = t0m1 - t2m3;
+				}
+			}
+			d += 2;
+			b <<= 2;
 		}
-	}
-	void _ntt_inv(vector<mint> &v) {
-		int n = v.size();
-		if (n == 1) return;
-		vector<mint> even(n/2), odd(n/2);
-		for (int i = 0; i < n/2; i++) {
-			even[i] = v[i*2];
-			odd[i] = v[i*2+1];
+		if (e & 1) {
+			b = n2 >> 1;
+			for (int i = 0; i < b; i++) {
+				mint s = v[i];
+				mint t = v[i+b] * root[i];
+				v[i] = s + t;
+				v[i+b] = s - t;
+			}
 		}
-		_ntt_inv(even);
-		_ntt_inv(odd);
-		for (int i = 0; i < n/2; i++) {
-			odd[i] *= _root[_n2-_n2/n*i];
-			v[i] = even[i] + odd[i];
-			v[n/2+i] = even[i] - odd[i];
-		}
-		for (int i = 0; i < n; i++) v[i] *= INV2;
 	}
 };
-using convolution = static_convolution<998244353>;
+template<int mod> const static_mint<mod> ntt_friendly_convolution<mod>::PR = []() {
+	using mint = ntt_friendly_convolution<mod>::mint;
+	vector<int> primes;
+	int x = mod - 1;
+	for (int i = 2; (long long)i * i <= x; i++) {
+		if (x % i == 0) {
+			primes.push_back(i);
+			while (x % i == 0) x /= i;
+		}
+	}
+	if (x > 1) primes.push_back(x);
+	mt19937 mt(time(nullptr));
+	while (true) {
+		mint g = mt();
+		bool ok = true;
+		for (auto &x : primes) {
+			if (g.pow((mod-1)/x) == 1) {
+				ok = false;
+				break;
+			}
+		}
+		if (ok) return g;
+	}
+}();
+template<int mod>
+struct arbitrary_convoultion {
+	using mint = static_mint<mod>;
+	arbitrary_convoultion() {}
+	vector<mint> operator()(vector<mint> &a, vector<mint> &b) const {
+		vector<int> ia(a.size()), ib(b.size());
+		for (int i = 0; i < a.size(); i++) ia[i] = a[i].val();
+		for (int i = 0; i < b.size(); i++) ib[i] = b[i].val();
+		vector<int> ic0 = _conv0(ia, ib);
+		vector<int> ic1 = _conv1(ia, ib);
+		vector<int> ic2 = _conv2(ia, ib);
+		vector<mint> c(ic0.size());
+		for (int i = 0; i < c.size(); i++) {
+			int t0 = ic0[i];
+			int t1 = (long long)(ic1[i] - t0 + 469762049) * _r01 % 469762049;
+			int t2 = ((long long)(ic2[i] - t0 + 754974721) * _r02r12 + (long long)(-t1 + 754974721) * _r12) % 754974721;
+			c[i] = (long long)t0 + (long long)t1 * 167772161 + (long long)t2 * (167772161LL * 469762049LL % mod);
+		}
+		return c;
+	}
+	vector<int> operator()(vector<int> &a, vector<int> &b) const {
+		vector<int> c0 = _conv0(a, b);
+		vector<int> c1 = _conv1(a, b);
+		vector<int> c2 = _conv2(a, b);
+		vector<int> c(c0.size());
+		int w = 167772161LL * 469762049LL % mod;
+		for (int i = 0; i < c.size(); i++) {
+			int t0 = c0[i];
+			int t1 = (long long)(c1[i] - t0 + 469762049) * _r01 % 469762049;
+			int t2 = ((long long)(c2[i] - t0 + 754974721) * _r02r12 + (long long)(-t1 + 754974721) * _r12) % 754974721;
+			c[i] = ((long long)t0 + (long long)t1 * 167772161 + (long long)t2 * w) % mod;
+		}
+		return c;
+	}
+private:
+	const ntt_friendly_convolution<167772161> _conv0;
+	const ntt_friendly_convolution<469762049> _conv1;
+	const ntt_friendly_convolution<754974721> _conv2;
+	const int _r01 = static_mint<469762049>(167772161).inv().val();
+	const int _r02 = static_mint<754974721>(167772161).inv().val();
+	const int _r12 = static_mint<754974721>(469762049).inv().val();
+	const int _r02r12 = (long long)_r02 * _r12 % 754974721;
+};
+using convolution = ntt_friendly_convolution<998244353>;
+using convolution = arbitrary_convoultion<1000000007>;
