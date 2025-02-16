879edc336df3fd4e1017a06874cbcdc8d2be3d58
rii922 <147631072+rii922@users.noreply.github.com>
2025-02-16 13:50:21 +0900

    `dynamic_montgomery_mint64<-1>` を使わないように

--- miller_rabin.cpp
+++ miller_rabin.cpp
@@ -3,26 +3,25 @@
 using namespace std;
 
 bool miller_rabin(long long x) {
+	using _mint64 = dynamic_mint64<static_cast<int>(0xc56a4ffc)>;
 	if (x == 2) return true;
 	if (x < 2 || x % 2 == 0) return false;
-	mint64::set_mod(x);
+	_mint64::set_mod(x);
 	long long d = x - 1;
 	long long s = 0;
 	while (d % 2 == 0) {
 		d /= 2;
 		s++;
 	}
-	vector<long long> bases;
-	if (x < 1LL << 32) bases = {2, 7, 61};
-	else bases = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+	const vector<long long> bases = (x < 1LL << 32) ? vector<long long>{2, 7, 61} : vector<long long>{2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 	for (auto &b : bases) {
-		mint64 a = b;
+		_mint64 a = b;
 		if (a == 0) continue;
-		mint64 t = a.pow(d);
+		_mint64 t = a.pow(d);
 		if (t == 1) continue;
 		bool e = true;
 		for (int i = 0; i < s; i++) {
-			if (t == x - 1) {
+			if (t == -1) {
 				e = false;
 				break;
 			}
