c548a1e36305cb0ae1ccc3a15673aadea6f77576
rii922 <147631072+rii922@users.noreply.github.com>
2025-02-17 10:40:07 +0900

    説明を追加

--- union_find.cpp
+++ union_find.cpp
@@ -1,12 +1,48 @@
+/**
+ * @file union_find.cpp
+ * @author rii922
+ * @brief 素集合データ構造
+ * @date 2025-02-17
+ *
+ * Verified with:
+ * https://judge.yosupo.jp/submission/268143
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * @brief 素集合データ構造
+ *
+ */
 struct union_find {
+	/**
+	 * @brief コンストラクタ
+	 *
+	 * @param n 頂点数
+	 */
 	union_find(int n) : _d(n, -1) {}
+
+	/**
+	 * @brief 頂点 `v` を含む連結成分の根を返す。
+	 *
+	 * O(α(n))
+	 * @param v 頂点
+	 * @return 頂点 `v` を含む連結成分の根
+	 */
 	int find(int v) {
 		if (_d[v] < 0) return v;
 		return _d[v] = find(_d[v]);
 	}
+
+	/**
+	 * @brief 頂点 `u` と `v` を同じ連結成分に所属させる。
+	 *
+	 * O(α(n))
+	 * @param u 頂点
+	 * @param v 頂点
+	 * @return `u` と `v` が新たに連結になったなら `true` 、既に連結だったなら `false`
+	 */
 	bool unite(int u, int v) {
 		u = find(u);
 		v = find(v);
@@ -16,9 +52,26 @@ struct union_find {
 		_d[v] = u;
 		return true;
 	}
+
+	/**
+	 * @brief 頂点 `u` と `v` が同じ連結成分に属するかを判定する。
+	 *
+	 * O(α(n))
+	 * @param u 頂点
+	 * @param v 頂点
+	 * @return 頂点 `u` と `v` が同じ連結成分に属するかどうか
+	 */
 	bool same(int u, int v) {
 		return find(u) == find(v);
 	}
+
+	/**
+	 * @brief 頂点 `v` を含む連結成分の頂点数を返す。
+	 *
+	 * O(α(n))
+	 * @param v 頂点
+	 * @return 頂点 `v` を含む連結成分の頂点数
+	 */
 	int size(int v) {
 		return -_d[find(v)];
 	}
