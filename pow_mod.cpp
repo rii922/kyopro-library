e0047151a81a5ac167e4cc8426bd99cff340af69
rii922 <147631072+rii922@users.noreply.github.com>
2025-02-21 23:01:46 +0900

    initial commit

---
+++ pow_mod.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file pow_mod.cpp
+ * @author rii922
+ * @brief 冪乗の剰余を求める。
+ * @date 2025-02-21
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @brief 冪乗の剰余を求める。
+ *
+ * @tparam _int `mod` の型
+ * @tparam _long `_int` の倍の精度の整数型
+ * @param x 底
+ * @param y 指数
+ * @param mod 法
+ * @return 冪乗の剰余
+ */
+template<integral _int, integral _long>
+constexpr _int pow_mod(_long x, _long y, _int mod) {
+	if (y == 0) return 1;
+	_long res = pow_mod<_int, _long>(x, y/2, mod);
+	res = res*res%mod;
+	if (y % 2 == 1) res = res*x%mod;
+	return res;
+}
