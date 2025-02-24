7eeaeff253680d596659784e0b395e1044ed4a0f
rii922 <147631072+rii922@users.noreply.github.com>
2025-02-24 14:17:26 +0900

    `primitive_root(n)` を追加

--- mylib/primitive_root.hpp
+++ mylib/primitive_root.hpp
@@ -1,11 +1,11 @@
 /**
  * @file primitive_root.hpp
  * @author rii922
- * @brief `mod` の原始根を求められるよう、 `mint` および `mint64` を拡張する。
+ * @brief 原始根を求められるよう、 `mint` および `mint64` を拡張する。 `primitive_root(n)` で呼ぶこともできる。
  * @date 2025-02-24
  *
  * Verified with:
- * https://judge.yosupo.jp/submission/269060
+ * https://judge.yosupo.jp/submission/269502
  */
 
 #include <bits/stdc++.h>
@@ -17,7 +17,7 @@ using namespace std;
 /// EXPAND FROM HERE
 
 /**
- * @brief `mod` の最小の原始根を求める。
+ * @brief `mod` の最小の原始根を求める。 `mod` が素数である必要がある。
  *
  * @tparam _int `_uint` と同じ精度の符号付き整数型
  * @tparam _uint `mod*4` を表現可能な符号無し整数型
@@ -29,6 +29,7 @@ using namespace std;
 template<gnu_signed_integral _int, gnu_unsigned_integral _uint, gnu_signed_integral _long, gnu_unsigned_integral _ulong, _uint mod>
 requires (sizeof(_int) == sizeof(_uint)) && (sizeof(_long) == sizeof(_ulong)) && (sizeof(_int)*2 == sizeof(_long)) && (mod < _uint(1) << (sizeof(_uint)*8 - 2)) && (mod % 2 == 1)
 constexpr static_montgomery_mint<_int, _uint, _long, _ulong, mod> static_montgomery_mint<_int, _uint, _long, _ulong, mod>::primitive_root() {
+	static_assert(_is_prime_mod);
 	using mint = static_montgomery_mint<_int, _uint, _long, _ulong, mod>;
 	unsigned long long x = mod - 1;
 	vector<_int> primes;
@@ -53,7 +54,7 @@ constexpr static_montgomery_mint<_int, _uint, _long, _ulong, mod> static_montgom
 }
 
 /**
- * @brief `mod` の原始根を 1 つ求める。乱択のため、返り値は呼び出しごとに変化しうる。
+ * @brief `mod` の原始根を 1 つ求める。乱択のため、返り値は呼び出しごとに変化しうる。 `mod` が素数である必要がある。
  *
  * @tparam _int `_uint` と同じ精度の符号付き整数型
  * @tparam _uint `mod*4` を表現可能な符号無し整数型
@@ -87,4 +88,17 @@ dynamic_montgomery_mint<_int, _uint, _long, _ulong, id> dynamic_montgomery_mint<
 		}
 		if (ok) return g;
 	}
+}
+
+/**
+ * @brief `n` の原始根を 1 つ求める。乱択のため、返り値は呼び出しごとに変化しうる。 `n` が素数である必要がある。
+ *
+ * @param n 素数
+ * @return `n` の原始根の 1 つ
+ */
+unsigned long long primitive_root(unsigned long long n) {
+	if (n == 2) return 1;
+	using mint64 = dynamic_mint64<static_cast<int>(0x6ac0e2b7)>;
+	mint64::set_mod(n);
+	return mint64::primitive_root().val();
 }
