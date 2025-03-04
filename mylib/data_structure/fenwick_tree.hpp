0edcc0940d00418c80eb6b3e1254080198b3487c
rii922 <147631072+rii922@users.noreply.github.com>
2025-03-04 20:36:31 +0900

    説明を追加

--- mylib/data_structure/fenwick_tree.hpp
+++ mylib/data_structure/fenwick_tree.hpp
@@ -1,15 +1,50 @@
+/**
+ * @file fenwick_tree.hpp
+ * @author rii922
+ * @brief 1 点更新および区間和の取得を高速に行う Fenwick 木
+ * @date 2025-03-04
+ *
+ * Verified with:
+ * https://judge.yosupo.jp/submission/271070
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 /// EXPAND FROM HERE
 
+/**
+ * @brief 1 点更新および区間和の取得を高速に行う Fenwick 木
+ *
+ * @tparam T 群
+ */
 template<class T>
 struct fenwick_tree {
-	fenwick_tree() {}
+	/**
+	 * @brief Fenwick 木をサイズ `n` で初期化する。要素はすべて 0 である。
+	 *
+	 * O(n)
+	 * @param n サイズ
+	 */
 	fenwick_tree(int n) : _v(n+1, T(0)) {}
+
+	/**
+	 * @brief Fenwick 木を `v` で初期化する。
+	 *
+	 * O(n)
+	 * @param v 初期化に使う配列
+	 */
 	fenwick_tree(vector<T> &v) : _v(v.size()+1, T(0)) {
 		for (int i = 0; i < _v.size(); i++) add(i, v[i]);
 	}
+
+	/**
+	 * @brief インデックス `p` に `x` を加算する。
+	 *
+	 * O(log n)
+	 * @param p インデックス
+	 * @param x 値
+	 */
 	void add(int p, T x) {
 		p++;
 		while (p < _v.size()) {
@@ -17,12 +52,36 @@ struct fenwick_tree {
 			p += p & -p;
 		}
 	}
+
+	/**
+	 * @brief インデックス `p` に `x` を代入する。
+	 *
+	 * O(log n)
+	 * @param p インデックス
+	 * @param x 値
+	 */
 	void set(int p, T x) {
 		add(p, x-get(p));
 	}
+
+	/**
+	 * @brief インデックス `p` の値を取得する。
+	 *
+	 * O(log n)
+	 * @param p インデックス
+	 * @return インデックス `p` の値
+	 */
 	T get(int p) {
 		return sum(p, p+1);
 	}
+
+	/**
+	 * @brief 区間 `[0, r)` の要素の総和 `a[0]+a[1]+...+a[r-1]` を取得する。
+	 *
+	 * O(log n)
+	 * @param r 右端
+	 * @return 区間 `[0, r)` の要素の総和
+	 */
 	T sum(int r) {
 		T res = 0;
 		while (r > 0) {
@@ -31,6 +90,15 @@ struct fenwick_tree {
 		}
 		return res;
 	}
+
+	/**
+	 * @brief 区間 `[l, r)` の要素の総和 `a[l]+a[l+1]+...+a[r-1]` を取得する。
+	 *
+	 * O(log n)
+	 * @param l 左端
+	 * @param r 右端
+	 * @return 区間 `[l, r)` の要素の総和
+	 */
 	T sum(int l, int r) {
 		return sum(r) - sum(l);
 	}
--- mylib/data_structure/segtree.hpp
+++ mylib/data_structure/segtree.hpp
@@ -1,23 +1,70 @@
+/**
+ * @file segtree.hpp
+ * @author rii922
+ * @brief 1 点更新および区間の要素の総積の取得を高速に行うセグメント木
+ * @date 2025-03-04
+ *
+ * Verified with:
+ * https://judge.yosupo.jp/submission/271067
+ * https://judge.yosupo.jp/submission/271081
+ */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 /// EXPAND FROM HERE
 
+/**
+ * @brief 1 点更新および区間の要素の総積の取得を高速に行うセグメント木
+ *
+ * @tparam S モノイド
+ * @tparam op 積
+ * @tparam e 単位元
+ */
 template<class S, S (*op)(S, S), S (*e)()>
 struct segtree {
+	/**
+	 * @brief セグメント木をサイズ `n` で初期化する。要素はすべて `e()` である。
+	 *
+	 * O(n)
+	 * @param n サイズ
+	 */
 	segtree(int n) {
 		_init(n);
 	}
+
+	/**
+	 * @brief セグメント木を `v` で初期化する。
+	 *
+	 * O(n)
+	 * @param v 初期化に使う配列
+	 */
 	segtree(vector<S> &v) {
 		_init(v.size());
 		for (int i = 0; i < _sz; i++) _node[_n+i-1] = v[i];
 		for (int i = _n-2; i >= 0; i--) _node[i] = op(_node[i*2+1], _node[i*2+2]);
 	}
+
+	/**
+	 * @brief セグメント木をサイズ `n` で初期化する。要素はすべて `x` となる。
+	 *
+	 * O(n)
+	 * @param n サイズ
+	 * @param x 初期値
+	 */
 	segtree(int n, S x) {
 		_init(n);
 		for (int i = 0; i < _sz; i++) _node[_n+i-1] = x;
 		for (int i = _n-2; i >= 0; i--) _node[i] = op(_node[i*2+1], _node[i*2+2]);
 	}
+
+	/**
+	 * @brief インデックス `p` に `x` を代入する。
+	 *
+	 * O(log n)
+	 * @param p インデックス
+	 * @param x 値
+	 */
 	void set(int p, S x) {
 		assert(0 <= p && p < _sz);
 		p += _n - 1;
@@ -27,22 +74,64 @@ struct segtree {
 			_node[p] = op(_node[p*2+1], _node[p*2+2]);
 		}
 	}
+
+	/**
+	 * @brief インデックス `p` の値を取得する。
+	 *
+	 * O(1)
+	 * @param p インデックス
+	 * @return インデックス `p` の値
+	 */
 	S get(int p) {
 		assert(0 <= p && p < _sz);
 		return _node[_n+p-1];
 	}
+
+	/**
+	 * @brief 区間 `[l, r)` の要素の総積 `a[l]*a[l+1]*...*a[r-1]` を取得する。
+	 *
+	 * O(log n)
+	 * @param l 左端
+	 * @param r 右端
+	 * @return 区間 `[l, r)` の要素の総積
+	 */
 	S prod(int l, int r) {
 		assert(0 <= l && l <= r && r <= _sz);
 		return _prod(l, r, 0, 0, _n);
 	}
+
+	/**
+	 * @brief 全要素の総積 `a[0]*a[1]*...*a[n-1]` を取得する。
+	 *
+	 * O(1)
+	 * @return 全要素の総積
+	 */
 	S all_prod() {
 		return _node[0];
 	}
+
+	/**
+	 * @brief (`r=l` または `f(a[l]*a[l+1]*...*a[r-1])=true`) かつ (`r=n` または `f(a[l]*a[l+1]*...*a[r])=false`) なる `r` を 1 つ返す。 `f(e())=true` である必要がある。
+	 *
+	 * `f` の計算量を定数時間として O(log n)
+	 * @param l 左端
+	 * @param f `S` から `bool` への関数で、 `f(e())=true`
+	 * @return (`r=l` または `f(a[l]*a[l+1]*...*a[r-1])=true`) かつ (`r=n` または `f(a[l]*a[l+1]*...*a[r])=false`) なる `r`
+	 */
 	int max_right(int l, bool (*f)(S)) {
 		assert(0 <= l && l <= _sz);
 		S pr = e();
 		return _max_right(l, f, 0, 0, _n, pr);
 	}
+
+	/**
+	 * @brief (`l=r` または `f(a[l]*a[l+1]*...*a[r-1])=true`) かつ (`l=0` または `f(a[l-1]*a[l]*...*a[r-1])=false`) なる `l` を 1 つ返す。 `f(e())=true` である必要がある。
+	 *
+	 * `f` の計算量を定数時間として O(log n)
+	 * @param r 右端
+	 * @param f `S` から `bool` への関数で、 `f(e())=true`
+	 * @return (`l=r` または `f(a[l]*a[l+1]*...*a[r-1])=true`) かつ (`l=0` または `f(a[l-1]*a[l]*...*a[r-1])=false`) なる `l`
+	 */
 	int min_left(int r, bool (*f)(S)) {
 		assert(0 <= r && r <= _sz);
 		S pr = e();
